fc692ecc8c184652f5a62ca913f88f458c1f85d6
Philippe Teixeira <philippe.teixeira@gmail.com>
2023-07-27 23:36:22 +0200

    routes to server

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8080/api/auth/'
+const API_URL = 'https://mighty-falls-43184-1878087393ca.herokuapp.com/api/auth/'
 
 class AuthService {
     login(user) {
--- src/services/game.service.js
+++ src/services/game.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8080/api/game/'
+const API_URL = 'https://mighty-falls-43184-1878087393ca.herokuapp.com/api/game/'
 
 class GameService {
     addGame(game) {
